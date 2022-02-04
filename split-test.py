e2e92065e7a45cfaaf07f23b390e1852859f36bd
Jim <jim@jam.sessionsnet.org>
2022-02-03 18:52:09 -0800

    add second test, where string has complete line at end

--- split-test.py
+++ split-test.py
@@ -1,3 +1,15 @@
 buffer = "hello\r\nworld\r\nparti"
 lines = buffer.split("\r\n")
 print(repr(lines))
+buffer = lines.pop()
+print(repr(lines))
+print("the buffer is: |" + buffer + "|")
+
+print("second test:")
+
+buffer = "hello\r\nworld\r\nnot partial\r\n"
+lines = buffer.split("\r\n")
+print(repr(lines))
+buffer = lines.pop()
+print(repr(lines))
+print("the buffer is: |" + buffer + "|")
