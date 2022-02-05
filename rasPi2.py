2c980dfb8abbfe61eead0555f0ec651c2b57f1ca
Jim <jim@jam.sessionsnet.org>
2022-02-05 05:44:41 -0800

    get rid of debug prints in func doReadConfig()

--- rasPi2.py
+++ rasPi2.py
@@ -39,8 +39,6 @@ def doReadConfig(path):
     parser = ConfigParser()
     result = parser.read(path)
 
-    print(f"doReadConfig output is {repr(result)}")
-    print(f"configFilePath is {repr(path)}")
     if dex(path, result) == -1:
         print(f"ERROR, did not find {path} in parser output")
         exit()
