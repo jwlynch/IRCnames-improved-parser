070231646f190a74ee14b05daf5a916762ba557d
Jim <jim@jam.sessionsnet.org>
2022-02-07 11:11:54 -0800

    break down (with comments) the print() for the raw irc line

--- test-parser.py
+++ test-parser.py
@@ -4,7 +4,12 @@ f = open("little-raw-log.txt", "r")
 
 for line in f:
     print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
-    print(f"the line: {line.rstrip()}")
 
     print(f"parsed: {repr(raw_line_parse(line))}")
+    # the line without return/linefeed at the end (rstrip takes them out)
+    line_no_crlf = line.rstrip()
+
+    # print adds its own crlf at the end of what it's printing
+    print(f"the line: {line_no_crlf}")
+
 print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
